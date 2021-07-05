4729e9f6e18247f0eb3b50e4902d047629df9395
unknown <andresjbo20@gmail.com>
2021-07-05 15:04:01 -0600

    trabajo en clase

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,94 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Loading } from './loading';
+import { User as UserList, UserProps } from "./User";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const User: React.FC<{ name?: string }> = ({ name }) => {
+
+  if (!name) {
+    return null;
+  }
+
+  return (<div>
+    <h1>{name}</h1>
+  </div>)
+}
+
+
+
+class App extends React.Component {
+  state = {
+    isUserLoggin: false,
+    age: 88,
+    loading: true,
+    isError: false,
+    users: [
+      {
+        name: "Luis",
+        age: "19",
+        color: "#fff"
+      },
+      {
+        name: "Andres",
+        age: "19",
+        color: "#fff"
+      },
+      {
+        name: "Jennifer",
+        age: "19",
+        color: "#fff"
+      },
+      {
+        name: "Diego",
+        age: "19",
+        color: "#fff"
+      },
+    ]
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ loading: false })
+    }, 3000);
+
+  }
+
+  addUser(user:UserProps){
+    this.setState({users:[...this.state.users, user]})
+  }
+
+  render() {
+    const ageMessage = this.state.age > 50 ? `ciudadano de oro` : `adulto joven`;
+    const { loading, isError, users } = this.state;
+    const filteredUsers = users.filter(user => user.name !== "Jennifer");
+
+    return (
+      <div className="container">
+        {loading ? (
+          <div>
+            <Loading />
+          </div>
+        ) : isError ? (
+          <div>
+            <p>Ups! Algo salio mal</p>
+
+          </div>
+        ) :
+          <div>
+            <p>Bienvenid@ a nuestro app</p>
+            <p>Tipo {ageMessage}</p>
+            <User name="Andres" />
+          </div>}
+        <div>
+          {filteredUsers.map((user, index) => <UserList key={index} {...user} />)}
+          <button onClick={() => this.addUser({ name: "Andrea", age: "30", color: "#000" })}>Add user!</button>
+        </div>
+
+
+      </div>
+    );
+  }
+
 }
 
 export default App;
